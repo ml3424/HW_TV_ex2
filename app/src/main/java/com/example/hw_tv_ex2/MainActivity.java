05104a83366a0213bd6c662dc3b6e5cf3ede11b7
ml3424 <ml3424@bs.amalnet.k12.il>
2023-12-19 09:09:25 +0200

    the button and textview

--- app/src/main/java/com/example/hw_tv_ex2/MainActivity.java
+++ app/src/main/java/com/example/hw_tv_ex2/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.hw_tv_ex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button btn;
+    TextView tV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+    }
+
+    public void click(View view) {
+
     }
 }
